f05899eed86627b6f9fbd2e067bd288f2863996c
Lasse Kathke <lasse@kathke.eu>
2019-07-16 13:37:41 +0200

    Second Commit

---
+++ Assets/CameraMatrixTransformations/NearViewPortCoordinates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+    public class NearViewPortCoordinates
+    {
+        public NearViewPortCoordinates(Camera camera)
+        {
+  
+            TopLeft = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+            TopRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, camera.nearClipPlane));
+            BottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, camera.nearClipPlane));
+            BottomRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+
+            XVector = TopRight - TopLeft;
+            YVector = BottomLeft - TopLeft;
+        }
+
+        public Vector3 TopLeft { get; private set; }
+        public Vector3 TopRight { get; private set; }
+        public Vector3 BottomLeft { get; private set; }
+        public Vector3 BottomRight { get; private set; }
+        public Vector3 XVector { get; private set; }
+        public Vector3 YVector { get; private set; }
+    }
+}
